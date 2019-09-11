35733f32e7eb7266a6ccd36cfb66d0ac77487e4f
davidf <david.forstenlechner@gmx.at>
2019-09-11 09:34:33 +0200

    Added support for nat/extip to the testnet setup script

--- pumba/setup_testnet.py
+++ pumba/setup_testnet.py
@@ -15,7 +15,11 @@ print('Generating Docker config volume folders for {num_nodes} hbbft nodes'.form
 
 generator_dir = '../../parity-ethereum/ethcore/hbbft_engine/hbbft_config_generator'
 
-run_cmd(['cargo', 'run', str(num_nodes), "Docker"], generator_dir)
+cmd = ['cargo', 'run', str(num_nodes), "Docker"]
+if len(sys.argv) > 2:
+    cmd.append(sys.argv[2])
+
+run_cmd(cmd, generator_dir)
 
 # Set up validator nodes
 for i in range(1, num_nodes + 1):
@@ -23,11 +27,11 @@ for i in range(1, num_nodes + 1):
     os.makedirs("containers/node{}/data/network".format(i), exist_ok=True)
     copyfile(generator_dir + "/hbbft_validator_{}.toml".format(i), "containers/node{}/node.toml".format(i))
     copyfile(generator_dir + "/hbbft_validator_key_{}".format(i), "containers/node{}/data/network/key".format(i))
-    copyfile(generator_dir + "/reserved_peers", "containers/node{}/reserved-peers".format(i))
+    copyfile(generator_dir + "/reserved-peers", "containers/node{}/reserved-peers".format(i))
     copyfile("../../posdao-test-setup/spec/spec.json", "containers/node{}/spec.json".format(i))
     
 # Set up Rpc node
 os.makedirs("containers/rpc_node", exist_ok=True)
 copyfile(generator_dir + "/rpc_node.toml", "containers/rpc_node/node.toml")
-copyfile(generator_dir + "/reserved_peers", "containers/rpc_node/reserved-peers")
+copyfile(generator_dir + "/reserved-peers", "containers/rpc_node/reserved-peers")
 copyfile("../../posdao-test-setup/spec/spec.json", "containers/rpc_node/spec.json")
