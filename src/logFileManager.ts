a77d38f9ef463b06d8e88f5ade4e23241fff5ad0
Thomas Haller <Thomas.Haller@lab10.coop>
2020-03-29 23:49:48 +0200

    ensuring output directory - logging JSON as well.

--- src/logFileManager.ts
+++ src/logFileManager.ts
@@ -27,11 +27,28 @@ export class LogFileManager {
         }
     }
 
+    public static ensureOutputPath() {
+        if (fs.existsSync(this.getOutputDirectory()))
+        {
+            fs.mkdirSync(this.getOutputDirectory());
+        }
+    }
+
     public static getOutputPathCSV() {
         return `${LogFileManager.getOutputDirectory()}/${LogFileManager.getLogConfigName()}${LogFileManager.getFileExtensionCSV()}`;
     }
+    
+    public static getOutputPathJSON() {
+        return `${LogFileManager.getOutputDirectory()}/${LogFileManager.getLogConfigName()}${LogFileManager.getFileExtensionJSON()}`;
+    }
 
     public static writeCSVOutput(csv: string) {
+        LogFileManager.ensureOutputPath();
         fs.writeFileSync(this.getOutputPathCSV(), new Buffer(csv));
+    }
+
+    public static writeJSONOutput(json: string) {
+        LogFileManager.ensureOutputPath();
+        fs.writeFileSync(this.getOutputPathJSON(), new Buffer(json));
     }
 }
